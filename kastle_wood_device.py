89f518ad394d526476d417177e58bf2fd3e6a4f5
Michael DeGaetano <digi@Valyria.local>
2016-04-17 16:08:48 -0400

    Edit kastle_wood_device to make the path shorter

--- kastle_wood_device.py
+++ kastle_wood_device.py
@@ -1,4 +1,4 @@
-# This scrilocation is for Krieger's telnet Kastle game at figgis.agency and will
+# This sript is for Krieger's telnet Kastle game at figgis.agency and will
 # automate the game to location 25 on the map and hold the device with wheel to
 # open the door at location 26
 #
@@ -8,7 +8,7 @@
 import telnetlib
 import time
 
-def do(actions, t=.4, disp_txt=1):
+def do(actions, t=.3, disp_txt=0):
     if type(actions) == str:
         actions = [actions]
     for action in actions:
@@ -25,28 +25,18 @@ print tn.read_very_eager()
 time.sleep(8)
 
 # take stick
-do('take stick')
+do(['take stick', 'e'])
 
-# move east, north, north to location 3, get diskette and chainsaw
-do(['e', 'n', 'n', 'take diskette', 'take chainsaw'], disp_txt=0)
+# go north to location 4, get key, move south back to location 3
+do(list('nnn') + ['take key', 's'])
 
-# go north to location 4, get gas & key, move south back to location 3
-do(['n', 'take gas', 'take key', 's'], disp_txt=0)
+# go east 5x, open door
+do(list('eeeee'))
+do('use key on padlock', t=2, disp_txt=1)
 
-# go east 4x then north to location 6, get briefcase & muffin
-do(list('eeeen') + ['take briefcase', 'open briefcase'], disp_txt=0)
-
-# move south then east to location 5 & use key on padlock
-# wait 2 seconds for text to load
-do(['s', 'e'], disp_txt=0)
-do('use key on padlock', 2, disp_txt=0)
-
-# move east 5x to location 34 & get robot arm
-do(['e']*5 + ['take arm'], disp_txt=0)
-
-# go south 2x, west 2x, south 2x, east 4x, south 2x, east 2x, north 2x, east 2x,
-# north 2x, east 2x, south 2x, east 2x, south 6x
-do(list('sswwsseeeesseenneenneessee' + 's'*6), disp_txt=0)
+# go east 5x, south 2x, west 2x, south 2x, east 4x, south 2x, east 2x, north 2x,
+# east 2x, north 2x, east 2x, south 2x, east 2x, south 6x
+do(list('eeeeesswwsseeeesseenneenneessee' + 's'*6))
 
 print 'kriger is at the wooden device, type "use device" when other krieger' + \
       ' is ready to go through the door'
