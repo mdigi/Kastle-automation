52cbeb3bcd9cf7ee6c243c3ea638a97a7322bca9
unknown <mdegaetano@MDEGAETANO-LTW7.vicorpower.vicr.com>
2016-04-18 10:28:35 -0400

    Change do function in _40 and _42 files

--- kastle_42.py
+++ kastle_42.py
@@ -3,14 +3,14 @@
 import telnetlib
 import time
 
-def do(actions, t=.3, disp_txt=0):
+def do(actions, t=0, disp_txt=0):
     if type(actions) == str:
         actions = [actions]
-    for action in actions:
-        tn.write(action + '\n')
-        time.sleep(t)
-        if disp_txt:
-            print tn.read_very_eager()
+    actions = ''.join(i + '\n' for i in actions)
+    tn.write(actions)
+    time.sleep(t)
+    if disp_txt:
+        print tn.read_very_eager()
 
 tn = telnetlib.Telnet('figgis.agency')
 time.sleep(.5)
@@ -19,19 +19,17 @@ tn.write('\n')
 print tn.read_very_eager()
 time.sleep(8)
 
-# take stick
-do(['take stick', 'e'])
+# take stick go east, go north 3x to location 4, get key, move south back to
+# location 3, go east 5x
+do(['take stick'] + list('ennn') + ['take key', 's'] + ['e']*5)
 
-# go north to location 4, get key, move south back to location 3
-do(list('nnn') + ['take key', 's'])
-
-# go east 5x, open door
-do(list('eeeee'))
-do('use key on padlock', t=2, disp_txt=1)
+# open door
+time.sleep(.3)
+do(['use key on padlock'], t=2, disp_txt=1)
 
 # go east 5x, south 2x, west 2x, south 2x, east 4x, south 2x, east 2x, north 2x,
-# east 2x, north 2x, east 2x, south 2x, east 2x, south 7x
-do(list('eeeeesswwsseeeesseenneenneessee' + 's'*7))
+# east 2x, north 2x, east 2x, south 2x, east 2x, south 6x
+do(list('eeeeesswwsseeeesseenneenneessee' + 's'*7), t=.3, disp_txt=1)
 
 print tn.read_very_eager()
 print 'At wooden door'
@@ -39,12 +37,12 @@ x = raw_input('press enter when other krieger clone opens up door')
 
 # go south, west 2x, north 2x, west 2x, north 2x, west 2x, south 2x, west 2x,
 # south 2x, west 2x, south to location 43 & get keycard
-do(list('swwnnwwnnwwsswwsswws'))
-do(['take card'])
+do(list('swwnnwwnnwwsswwsswws'), t=.3)
+do(['take card'], t=.3, disp_txt=1)
 
 # go north, east 2x, north 2x, east 2x, north 2x, east 2x, south 2x, east 2x,
 # south 2x, east 2x to door under 26
-do(list('neenneenneesseessee'))
+do(list('neenneenneesseessee'), t=.3, disp_txt=1)
 
 print tn.read_very_eager()
 print 'At wooden door'
@@ -52,10 +50,10 @@ x = raw_input('press enter when other krieger clone opens up door')
 
 # go north 8x, west 2x, north 2x, west 2x, south 2x, west 2x, south 2x, west 2x,
 # north 2x, west 2x, south 2x, west 2x, south 6x to location 12
-do(list('n'*8 + 'wwnnwwsswwsswwnnwwssww' + 's'*6))
+do(list('n'*8 + 'wwnnwwsswwsswwnnwwssww' + 's'*6), t=.3)
 
 # open lab door
-do('use card on door', 1)
+do('use card on door', t=1, disp_txt=1)
 
 # go west 5x, south 2x, east 2x
 do(list('w'*5 + 'ssee'))
