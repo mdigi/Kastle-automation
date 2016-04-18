eb849124c6a5e0a3479950d6c308ae5677ea1cc6
unknown <mdegaetano@MDEGAETANO-LTW7.vicorpower.vicr.com>
2016-04-18 16:24:30 -0400

    Add command method to krieger files to gain access to do function in object. Add krieger_1 which is the krieger that will collect all items in the map.

--- krieger_button.py
+++ krieger_button.py
@@ -8,8 +8,8 @@
 import telnetlib
 import time
 from do import do
-       
-class Krieger_Button:        
+
+class Krieger_Button:
     '''
     Create a Krieger that will be at location 38, 40, or 42 to push button
     type is 38, 40, or 42
@@ -17,25 +17,25 @@ class Krieger_Button:
     '''
     def __init__(self, type, krieger_wood):
         self.type = str(type)
-        
+
         self.tn = telnetlib.Telnet('figgis.agency')
         time.sleep(.5)
 
         self.tn.write('\n')
         print self.tn.read_very_eager()
         time.sleep(8)
-        
+
         # take stick go east, go north 3x to location 4, get key, move south back to
         # location 3, go east 5x
-        do(self.tn, ['take stick'] + list('ennn') + ['take key', 's'] + ['e']*5)
+        self.command(['take stick'] + list('ennn') + ['take key', 's'] + ['e']*5)
 
         # open door
         time.sleep(.3)
-        do(self.tn, ['use key on padlock'], t=2, disp_txt=1)
+        self.command(['use key on padlock'], t=2, disp_txt=1)
 
         # go east 5x, south 2x, west 2x, south 2x, east 4x, south 2x, east 2x, north 2x,
         # east 2x, north 2x, east 2x, south 2x, east 2x, south 6x
-        do(self.tn, list('eeeeesswwsseeeesseenneenneessee' + 's'*7), t=.3, disp_txt=1)
+        self.command(list('eeeeesswwsseeeesseenneenneessee' + 's'*7), t=.3, disp_txt=1)
 
         # go into cave
         print 'At wooden door'
@@ -44,12 +44,12 @@ class Krieger_Button:
 
         # go south, west 2x, north 2x, west 2x, north 2x, west 2x, south 2x, west 2x,
         # south 2x, west 2x, south to location 43 & get keycard
-        do(self.tn, list('swwnnwwnnwwsswwsswws'), t=.3)
-        do(self.tn, ['take card'], t=.3, disp_txt=1)
+        self.command(list('swwnnwwnnwwsswwsswws'), t=.3)
+        self.command(['take card'], t=.3, disp_txt=1)
 
         # go north, east 2x, north 2x, east 2x, north 2x, east 2x, south 2x, east 2x,
         # south 2x, east 2x to door under 26
-        do(self.tn, list('neenneenneesseessee'), t=.3, disp_txt=1)
+        self.command(list('neenneenneesseessee'), t=.3, disp_txt=1)
 
         # leave cave, don't need to ask other krieger to open door as commands
         # should be fast enough that the door should still be open
@@ -57,44 +57,48 @@ class Krieger_Button:
 
         # go north 8x, west 2x, north 2x, west 2x, south 2x, west 2x, south 2x, west 2x,
         # north 2x, west 2x, south 2x, west 2x, south 6x to location 12
-        do(self.tn, list('n'*8 + 'wwnnwwsswwsswwnnwwssww' + 's'*6), t=.3)
+        self.command(list('n'*8 + 'wwnnwwsswwsswwnnwwssww' + 's'*6), t=.3)
 
         # open lab door
-        do(self.tn, 'use card on door', t=1, disp_txt=1)
-                          
+        self.command('use card on door', t=1, disp_txt=1)
+
         if self.type == '38':
             # go west 5x, north 10x, east 2x, south 2x
-            do(self.tn, list('w'*5 + 'n'*10 + 'eess'), disp_txt=1)
+            self.command(list('w'*5 + 'n'*10 + 'eess'), disp_txt=1)
 
             print 'at location 38'
-            
+
         if self.type == '40':
             # go west 5x, north 8x, west 2x, south 2x
-            do(self.tn, list('w'*5 + 'n'*8 + 'wwss'), disp_txt=1)
+            self.command(list('w'*5 + 'n'*8 + 'wwss'), disp_txt=1)
 
             print 'at location 40'
-            
+
         if self.type == '42':
             # go west 5x, south 2x, east 2x
-            do(self.tn, list('w'*5 + 'ssee'), disp_txt=1)
+            self.command(list('w'*5 + 'ssee'), disp_txt=1)
 
             print 'at location 42'
-              
+
     def check(self):
         'Press return and print out to console to check game stats'
-        do(self.tn, '', t=.3, disp_txt=1)
-        
+        self.command('', t=.3, disp_txt=1)
+
+    def command(self, actions, t=0, disp_txt=0):
+        'Use do function'
+        do(self.tn, actions, t, disp_txt)
+
     def interact(self):
         'Enter the telnet session'
         self.tn.interact()
-    
+
     def push_button(self):
         'Tell Krieger to push the button on the console'
-        do(self.tn, 'push button', t=.3)
-        
+        self.command('push button', t=.3)
+
     def quit(self):
         self.tn.write('quit\n')
         time.sleep(.3)
         self.tn.write('exit\n')
-    
+
 # tn.close()
--- krieger_wood_device.py
+++ krieger_wood_device.py
@@ -8,46 +8,50 @@
 import telnetlib
 import time
 from do import do
-       
-class Krieger_Wood_Device:       
+
+class Krieger_Wood_Device:
     'Create a Krieger that goes to location 25 on the map and is ready to use device'
-    def __init__(self):       
+    def __init__(self):
         self.tn = telnetlib.Telnet('figgis.agency')
         time.sleep(.5)
 
         self.tn.write('\n')
         print self.tn.read_very_eager()
         time.sleep(8)
-        
+
         # take stick go east, go north 3x to location 4, get key, move south back to
         # location 3, go east 5x
-        do(self.tn, ['take stick'] + list('ennn') + ['take key', 's'] + ['e']*5)
+        self.command(['take stick'] + list('ennn') + ['take key', 's'] + ['e']*5)
 
         # open door
         time.sleep(.3)
-        do(self.tn, ['use key on padlock'], t=2, disp_txt=1)
+        self.command(['use key on padlock'], t=2, disp_txt=1)
 
         # go east 5x, south 2x, west 2x, south 2x, east 4x, south 2x, east 2x, north 2x,
         # east 2x, north 2x, east 2x, south 2x, east 2x, south 6x
-        do(self.tn, list('eeeeesswwsseeeesseenneenneessee' + 's'*6))
+        self.command(list('eeeeesswwsseeeesseenneenneessee' + 's'*6))
 
         print 'kriger is at the wooden device, ready to turn the damn wheel'
-             
+
     def check(self):
         'Press return and print out to console to check game stats'
-        do(self.tn, '', t=.3, disp_txt=1)
-        
+        self.command('', t=.3, disp_txt=1)
+
+    def command(self, actions, t=0, disp_txt=0):
+        'Use do function'
+        do(self.tn, actions, t, disp_txt)
+
     def interact(self):
         'Enter the telnet session'
         self.tn.interact()
-    
+
     def open_door(self):
         'Tell Krieger to use the wooden device and open the cave door'
-        do(self.tn, 'use device\n', t=.3, disp_txt=1)
-        
+        self.command('use device\n', t=.3, disp_txt=1)
+
     def quit(self):
         self.tn.write('quit\n')
         time.sleep(.3)
         self.tn.write('exit\n')
-    
+
 # tn.close()
