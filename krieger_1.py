0bf8054e89bca6270af7cbe1e9286f226378a3e0
Michael DeGaetano <digi@Valyria.local>
2016-04-18 18:30:42 -0400

    krieger_1 puts Krieger above the robot, it works now

--- krieger_1.py
+++ krieger_1.py
@@ -8,18 +8,18 @@
 import telnetlib
 import time
 from do import do
+from create_krieger import Krieger
 
-class Krieger_1:
+class Krieger_1(Krieger):
     '''
     Create a Krieger that gets all the items
     '''
     def __init__(self, krieger_wood):
-        self.tn = telnetlib.Telnet('figgis.agency')
-        time.sleep(.5)
+        Krieger.__init__(self, krieger_type='')
+        self.krieger_wood = krieger_wood
+        self.go()
 
-        self.tn.write('\n')
-        print self.tn.read_very_eager()
-        time.sleep(8)
+    def go(self):
 
         # take stick, go east, north 2x to location 3 take diskette & chainsaw
         self.command(['take stick'] + list('enn') + ['take diskette', 'take chainsaw'], t=.3)
@@ -104,7 +104,7 @@ class Krieger_1:
 
         # go into cave
         print 'At wooden door'
-        krieger_wood.open_door()
+        self.krieger_wood.open_door()
         time.sleep(3)
 
         # go south, west 2x, north 2x, west 2x, north 2x, west 2x, south 2x, west 2x,
@@ -119,143 +119,121 @@ class Krieger_1:
         # leave cave, don't need to ask other krieger to open door as commands
         # should be fast enough that the door should still be open
         print 'At wooden door'
-        krieger_wood.open_door()
+        self.krieger_wood.open_door()
         time.sleep(3)
 
         # go north 8x, west 2x, north 2x, west 2x, south 2x, west 2x, south 2x, west 2x,
         # north 2x, west 2x, south 2x, west 2x, south 6x to location 12
         self.command(list('n'*8 + 'wwnnwwsswwsswwnnwwssww' + 's'*6), t=.3)
-        #
-        # # open lab door
-        # self.command('use card on door', t=1, disp_txt=1)
-        #
-        # # go west 5x, south 2x, east 2x to location 42 & get hand truck
-        # self.command(list('w'*5 + 'ssee') + ['take hand truck'], t=.3)
-        # self.command('take data tape', t=.3)
-        #
-        # if 0:
-        #     # this code is for using getting the computer back to location 42 to use
-        #     # the data tape
-        #     # go to location 21 & haul the computer back to 42
-        #     self.command(list('wwnn' + 'e'*6 + 'n'*6 + 'eenneess' + 'e'*6 + 'ss'))
-        #     self.command('take computer')
-        #
-        #     # go back to location 42
-        #     self.command(list('nn' + 'w'*6 + 'nnwwssww' + 's'*6))
-        #     self.command('use card on door', 1)
-        #     self.command(list('w'*5 + 'ssee'))
-        #
-        #     # get data tape
-        #     self.command(['use data tape on computer'])
-        #
-        #     tn.interact()
-        #
-        # # go west 2x, north 10x, west to location 39 & get slingshot
-        # self.command(list('ww' + 'n'*10 + 'w') + ['take slingshot'], t=.3)
-        #
-        # # go west, south 2x to location 40 & get crock pot
-        # self.command(list('wss') + ['take pot'], t=.3)
-        #
-        # # go north 2x, east 2x, north to location 44 & get robot leg
-        # self.command(list('nneen') + ['take leg'], t=.3)
-        #
-        # # go north, east, & get pinata
-        # self.command(['n', 'e', 'take pinata'], t=.3)
-        #
-        # # go east to location 45 & get punched card
-        # self.command(['e', 'take vials'], t=.3)
-        # self.command('take punched card', t=.3)
-        #
-        # # go south 2x to location 38 & take beer bottle
-        # self.command(['s', 's', 'take beer bottle'], t=.3)
-        #
-        # # go north 2x, west 2x, south 10x, east 6x, north 2x, east 2x, north 2x, east 2x
-        # # to location 16 & fill beer bottle with water
-        # self.command(list('nnww' + 's'*10 + 'e'*6 + 'nneennee') + ['use beer bottle on water'], t=.3)
-        #
-        # # go west 2x, south 2x, west 2x, north 4x, east 2x, north 2x, east 2x, south 2x,
-        # # east 2x, north 2x, east 2x, north 2x, east 2x, south 2x, east 2x, south 7x
-        # # to location 26 the wooden door
-        # self.command(list('wwsswwnnnneenneesseenneenneessee' + 's'*7), disp_txt=1)
-        #
-        # # enter cave #2 with bomb
-        # print 'At wooden door'
-        # krieger_wood.open_door()
-        # time.sleep(3)
-        #
-        # # go south, west 2x, north 2x, west 2x, north 2x, west 2x, south 2x, west 2x,
-        # # south 2x, west 2x, south to location 43 & make dry ice bomb
-        # self.command(list('swwnnwwnnwwsswwsswws') + ['use dry ice on beer bottle'], t=.3)
-        #
-        # # plant the bomb
-        # self.command('use bottle cap on dry ice bomb', t=.3, disp_txt=1)
-        # time.sleep(2)
-        #
-        # # go north, east & wait to avoid bomb, then west & south 4x to location 46
-        # self.command('n', t=1)
-        # self.command('e', t=4)
-        # self.command(list('w' + 's'*4))
-        #
-        # # take pen at location 46
-        # self.command('take pen', t=.3)
-        #
-        # # go west 2x, south 2x, west 2x to location 47 & get whiskey stones
-        # self.command(list('wwssww') + ['take whiskey stones'], t=.3)
-        #
-        # # go south 2x & get hole
-        # self.command(list('ss') + ['take hole'], t=.3)
-        #
-        # # go east 4x, north 2x, east 4x, north to location 49 & get candy wrapper
-        # self.command(list('eeeenneeeen') + ['take wrapper'], t=.3)
-        #
-        # # go north to location 50 & get foil swan
-        # self.command(['n', 'pick swan'], t=.3)
-        #
-        # # go south 2x, west 2x, north 2x to location 48 & get knife
-        # self.command(list('sswwnn') + ['take knife'], t=.3)
-        #
-        # # go north 2x, east 4x, south 2x to location 53 & get knitting needle
-        # self.command(list('nneeeess') + ['take needle'], t=.3)
-        #
-        # # go north 2x, east 4x, south 2x to location 54 & get spatula, hat
-        # self.command(list('nneeeess') + ['take spatula'], t=.3)
-        # self.command('take hat', t=.3)
-        #
-        # # go north 2x, west 2x, south 6x, east 2x to location 55 & get bratwurst
-        # self.command(list('nnww' + 's'*6 + 'ee') + ['take bratwurst'], t=.3)
-        #
-        # # go west 2x & take window
-        # self.command(list('ww') + ['take window'], t=.3)
-        #
-        # # # go north 6x, west 6x, south 4x, west 2x, souith 2x, west 4x, north 2x,
-        # # # east 2x, north 2x,  east 2x, north 4x
-        # # self.command(list('n'*6 + 'w'*6 + 'sssswwsswwwwnneenneennnn'))
-        #
-        # # go near robot
-        # self.command(list('n'*6 + 'wwss'), t=.3)
-        #
-        # print self.tn.read_very_eager()
-        # print 'near robot'
 
-    def check(self):
-        'Press return and print out to console to check game stats'
-        self.command('', t=.3, disp_txt=1)
+        # open lab door
+        self.command('use card on door', t=1, disp_txt=1)
 
-    def command(self, actions, t=0, disp_txt=0):
-        'Use do function'
-        do(self.tn, actions, t, disp_txt)
+        # go west 5x, south 2x, east 2x to location 42 & get hand truck
+        self.command(list('w'*5 + 'ssee') + ['take hand truck'], t=.3)
+        self.command('take data tape', t=.3)
 
-    def interact(self):
-        'Enter the telnet session'
-        self.tn.interact()
+        if 0:
+            # this code is for using getting the computer back to location 42 to use
+            # the data tape
+            # go to location 21 & haul the computer back to 42
+            self.command(list('wwnn' + 'e'*6 + 'n'*6 + 'eenneess' + 'e'*6 + 'ss'))
+            self.command('take computer')
 
-    def push_button(self):
-        'Tell Krieger to push the button on the console'
-        self.command('push button', t=.3)
+            # go back to location 42
+            self.command(list('nn' + 'w'*6 + 'nnwwssww' + 's'*6))
+            self.command('use card on door', 1)
+            self.command(list('w'*5 + 'ssee'))
 
-    def quit(self):
-        self.tn.write('quit\n')
-        time.sleep(.3)
-        self.tn.write('exit\n')
+            # get data tape
+            self.command(['use data tape on computer'])
 
-# tn.close()
+            tn.interact()
+
+        # go west 2x, north 10x, west to location 39 & get slingshot
+        self.command(list('ww' + 'n'*10 + 'w') + ['take slingshot'], t=.3)
+
+        # go west, south 2x to location 40 & get crock pot
+        self.command(list('wss') + ['take pot'], t=.3)
+
+        # go north 2x, east 2x, north to location 44 & get robot leg
+        self.command(list('nneen') + ['take leg'], t=.3)
+
+        # go north, east, & get pinata
+        self.command(['n', 'e', 'take pinata'], t=.3)
+
+        # go east to location 45 & get punched card
+        self.command(['e', 'take vials'], t=.3)
+        self.command('take punched card', t=.3)
+
+        # go south 2x to location 38 & take beer bottle
+        self.command(['s', 's', 'take beer bottle'], t=.3)
+
+        # go north 2x, west 2x, south 10x, east 6x, north 2x, east 2x, north 2x, east 2x
+        # to location 16 & fill beer bottle with water
+        self.command(list('nnww' + 's'*10 + 'e'*6 + 'nneennee') + ['use beer bottle on water'], t=.3)
+
+        # go west 2x, south 2x, west 2x, north 4x, east 2x, north 2x, east 2x, south 2x,
+        # east 2x, north 2x, east 2x, north 2x, east 2x, south 2x, east 2x, south 7x
+        # to location 26 the wooden door
+        self.command(list('wwsswwnnnneenneesseenneenneessee' + 's'*7), disp_txt=1)
+
+        # enter cave #2 with bomb
+        print 'At wooden door'
+        self.krieger_wood.open_door()
+        time.sleep(3)
+
+        # go south, west 2x, north 2x, west 2x, north 2x, west 2x, south 2x, west 2x,
+        # south 2x, west 2x, south to location 43 & make dry ice bomb
+        self.command(list('swwnnwwnnwwsswwsswws') + ['use dry ice on beer bottle'], t=.3)
+
+        # plant the bomb
+        self.command('use bottle cap on dry ice bomb', t=.3, disp_txt=1)
+        time.sleep(2)
+
+        # go north, east & wait to avoid bomb, then west & south 4x to location 46
+        self.command('n', t=1)
+        self.command('e', t=4)
+        self.command(list('w' + 's'*4))
+
+        # take pen at location 46
+        self.command('take pen', t=.3)
+
+        # go west 2x, south 2x, west 2x to location 47 & get whiskey stones
+        self.command(list('wwssww') + ['take whiskey stones'], t=.3)
+
+        # go south 2x & get hole
+        self.command(list('ss') + ['take hole'], t=.3)
+
+        # go east 4x, north 2x, east 4x, north to location 49 & get candy wrapper
+        self.command(list('eeeenneeeen') + ['take wrapper'], t=.3)
+
+        # go north to location 50 & get foil swan
+        self.command(['n', 'pick swan'], t=.3)
+
+        # go south 2x, west 2x, north 2x to location 48 & get knife
+        self.command(list('sswwnn') + ['take knife'], t=.3)
+
+        # go north 2x, east 4x, south 2x to location 53 & get knitting needle
+        self.command(list('nneeeess') + ['take needle'], t=.3)
+
+        # go north 2x, east 4x, south 2x to location 54 & get spatula, hat
+        self.command(list('nneeeess') + ['take spatula'], t=.3)
+        self.command('take hat', t=.3)
+
+        # go north 2x, west 2x, south 6x, east 2x to location 55 & get bratwurst
+        self.command(list('nnww' + 's'*6 + 'ee') + ['take bratwurst'], t=.3)
+
+        # go west 2x & take window
+        self.command(list('ww') + ['take window'], t=.3)
+
+        # # go north 6x, west 6x, south 4x, west 2x, souith 2x, west 4x, north 2x,
+        # # east 2x, north 2x,  east 2x, north 4x
+        # self.command(list('n'*6 + 'w'*6 + 'sssswwsswwwwnneenneennnn'))
+
+        # go near robot
+        self.command(list('n'*6 + 'wwss'), t=.3)
+
+        print self.tn.read_very_eager()
+        print 'near robot'
+
